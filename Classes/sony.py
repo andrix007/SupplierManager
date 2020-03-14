1652947c2d5f771156118f2f747fcde31c0576c5
Andrei Bancila <51709399+andrix007@users.noreply.github.com>
2020-03-14 12:59:32 +0200

    Done with the first supplier

--- Classes/sony.py
+++ Classes/sony.py
@@ -49,11 +49,51 @@ class Sony(MainApplication):
 
     def solve(self):
 
-        print(getFileXFromPath('C:\\Users\\Andrei Bancila\\Desktop\\Furnizori\\Plaseaza_fisierele_aici\\Catalog',1))
-        convertXlsToXlsx(getFileXFromPath('C:\\Users\\Andrei Bancila\\Desktop\\Furnizori\\Plaseaza_fisierele_aici\\Catalog',1))
-        print(getExtension(getFileXFromPath('C:\\Users\\Andrei Bancila\\Desktop\\Furnizori\\Plaseaza_fisierele_aici\\Catalog',1)))
+        error = open(MainApplication.univPath+"\\Resources"+"\\error.txt","w")
 
-        #mergeFiles('C:\\Users\\Andrei Bancila\\Desktop\\Furnizori\\Plaseaza_fisierele_aici\\Catalog','C:\\Users\\Andrei Bancila\\Desktop\\Furnizori\\Plaseaza_fisierele_aici\\Catalog',1,1)
+        file = self.supplierInfo['catalogue_path']
+        convertAll(file)
 
-        addFileToOtherFile(getFileXFromPath('C:\\Users\\Andrei Bancila\\Desktop\\Furnizori\\Plaseaza_fisierele_aici\\Catalog',1),getFileXFromPath('C:\\Users\\Andrei Bancila\\Desktop\\Furnizori\\Plaseaza_fisierele_aici\\Catalog',2),4)
+        save_path = self.supplierInfo['save_path']
+        start_row = self.supplierInfo['start_row']
+
+        file_workbook =  load_workbook(getFileXFromPath(file,1))
+        file_sheet = file_workbook.active
+
+        barcode_column = self.supplierInfo['barcode_column']
+        price_column = self.supplierInfo['price_column']
+
+        void_workbook = openpyxlWorkbook()
+        void_sheet = void_workbook.active
+        void_sheet.cell(row = 1,column = 1).value = "Barcode"
+        void_sheet.cell(row = 1,column = 6).value = "Price"
+
+        currentRow = 1
+        i = 0
+
+        for row in list(iter_rows(file_sheet)):
+            i = i + 1
+            if i < start_row:
+                continue
+            barcode = str(row[barcode_column-1])
+            price = row[price_column-1]
+
+            if barcode.isdigit() and isfloat(str(price)):
+
+                currentRow = currentRow + 1
+
+                barcode = barcode.zfill(13)
+                newPrice = price/1.19*0.6
+
+                void_sheet.cell(row = currentRow,column = 1).value = barcode
+                void_sheet.cell(row = currentRow,column = 6).value = round(newPrice,2)
+
+            else:
+
+                errorText = "Line " + str(i) + ":   " + barcode + " " + str(price) + "\n"
+                error.write(errorText)
+
+        error.close()
+        void_workbook.save(save_path+"\\void - cat.xlsx")
+
         self.master.destroy()
